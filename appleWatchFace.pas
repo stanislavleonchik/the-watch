e5155450dc4a8283987624ebc7a2e1905f5da30b
Stanislav Leonchik <leonstas1@mail.ru>
2021-10-24 01:53:42 +0300

    upd2

--- appleWatchFace.pas
+++ appleWatchFace.pas
@@ -11,11 +11,10 @@ var SecondArrow : lineWPF := nil;
 procedure watchFaceCreate;
 begin 
   SetStandardCoords(1.0,window.width / 2, window.Height / 2);
-//  circle(0, 0, window.height / 3);
   var counter := 0;
   var a := 0.0;
   while a <= PI + 0.1 do begin
-    if counter mod 5 = 0  then pen.Width := 5 else pen.Width := 1;
+    if counter mod 10 = 0  then pen.Width := 5 else pen.Width := 1;
     line(window.height / 3 * cos(a), window.height * sin(a) / 3, -window.height * cos(a) / 3, -window.height * sin(a) / 3);
     a += ungle;
     counter += 1;
@@ -39,12 +38,11 @@ begin
   var tempUngle := PI*2 / 60;
   var tempHourUngle := PI*2 / 12;
   while true do begin
+    sleep(10);
     var ct := System.DateTime.Now;
     var secondUngle := -PI / 2 + tempUngle * ct.second + (PI * 2 / 1000 / 60) * ct.Millisecond;
     var minuteUngle := -PI / 2 + tempUngle * ct.minute + (secondUngle + PI / 2) / 60;
     var hourUngle   := -PI / 2 + tempHourUngle * ct.hour + (minuteUngle + PI / 2) / 12;
-    
-    
     HourArrow.x2   := w + ClockWidth / 2 * cos(hourUngle);     HourArrow.y2 := h + ClockWidth / 2 * sin(hourUngle);
     MinuteArrow.x2 := w + ClockWidth / 3 * 2 * cos(minuteUngle); MinuteArrow.y2 := h + ClockWidth / 3 * 2 * sin(minuteUngle);
     SecondArrow.x2 := w + ClockWidth * cos(secondUngle); SecondArrow.y2 := h + ClockWidth * sin(secondUngle);
